300a5ae1887117839472a8519dc1c23f4fa193a9
Sumit70510 <sumit.verma.70510@gmail.com>
2026-05-05 21:28:21 +0530

    Name Change

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+   res.send('Hello World!'); 
+ });
+ 
+app.listen(4000,()=>{
+    console.log('Server started at 4000');
+}) 
