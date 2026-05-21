81e6b2c91cb7a9b1170b5d0f32492732b9ebd64e
Sumit70510 <sumit.verma.70510@gmail.com>
2026-05-21 18:38:08 +0530

    Frontend From Scratch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import HomePage from './pages/HomePage.jsx';
 import './App.css'
 
 function App() {
   return (
     <>
-     <h1>
-       FoodDash
-     </h1>
+     <HomePage/>
     </>
   )
 }
 
-export default App
+export default App;
